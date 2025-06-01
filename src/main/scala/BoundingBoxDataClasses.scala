b83c0a35a8bb94340c0cad458a3dade31c401ac4
Amizan Jaleel <najm555@gmail.com>
2025-05-31 20:00:37 -0400

    get rid of overrides equals, I didn't need that

--- src/main/scala/BoundingBoxDataClasses.scala
+++ src/main/scala/BoundingBoxDataClasses.scala
@@ -2,19 +2,10 @@ package org.amizan
 
 object BoundingBoxDataClasses {
   final case class Point(x: Int, y: Int) {
-    override def equals(obj: Any): Boolean = obj match {
-      case Point(x, y) => x == this.x && y == this.y
-      case _ => false
-    }
     override def toString: String = s"($x,$y)"
   }
 
   final case class Box(topLeft: Point, bottomRight: Point) {
-    override def equals(obj: Any): Boolean = obj match {
-      case Box(topLeft, bottomRight) =>
-        topLeft == this.topLeft && bottomRight == this.bottomRight
-      case _ => false
-    }
     override def toString: String = s"$topLeft$bottomRight"
     lazy val topRight: Point =
       Point(topLeft.x, bottomRight.y)
@@ -27,14 +18,8 @@ object BoundingBoxDataClasses {
     }
 
     def overlaps(other: Box): Boolean = {
-      if(this == other) {
-        // if the two boxes are equal, they don't 'overlap', they are the same
-        false
-      }
-      else {
-        val points = topLeft :: bottomRight :: topRight :: bottomLeft :: Nil
-        points.map(other.contains).exists(identity)
-      }
+      val points = topLeft :: bottomRight :: topRight :: bottomLeft :: Nil
+      points.map(other.contains).exists(identity)
     }
   }
 
